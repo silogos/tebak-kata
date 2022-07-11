a9487fcb16b8bb9ef528e3038506c8349fd597a5
Amin Yusuf <amin.yusuf@lemonilo.com>
2022-07-12 02:41:34 +0700

    init eslint and prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:testing-library/react',
+    'plugin:prettier/recommended',
+    'next/core-web-vitals',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 2,
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
